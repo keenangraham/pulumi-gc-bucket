cb28873d5ce446796d92e05ea464c13ebc65b7de
Keenan Graham <ksgraham@stanford.edu>
2025-04-11 15:50:26 -0700

    Add docker

--- __main__.py
+++ __main__.py
@@ -1,10 +1,14 @@
-import pulumi
+from pulumi import Output
+from pulumi import export
+from pulumi import Config
 
+from pulumi_docker import Image
 
 from pulumi_gcp import storage
 
 from pulumi_gcp.storage import get_transfer_project_service_account
 from pulumi_gcp.storage import BucketIAMMember
+from pulumi_gcp.artifactregistry import Repository
 
 
 storage_transfer_service_account = get_transfer_project_service_account()
@@ -12,13 +16,14 @@ storage_transfer_service_account = get_transfer_project_service_account()
 
 bucket = storage.Bucket(
     'test-pulumi-bucket',
-    location='US-WEST1',
+    location='us-west1',
     force_destroy=True,
     soft_delete_policy={
         'retention_duration_seconds': 0,
     }
 )
 
+gc_project = Config('gcp').require('project')
 
 member = BucketIAMMember(
     'member',
@@ -28,7 +33,38 @@ member = BucketIAMMember(
 )
 
 
+docker_registry = Repository(
+    'test-repo-pulumi',
+    location='us-west1',
+    repository_id='some-repo',
+    format='DOCKER'
+)
 
-pulumi.export('bucket_name', bucket.url)
-pulumi.export('storage_transfer_service_account_email', storage_transfer_service_account.email)
-pulumi.export('storage_transfer_service_account_subject_id', storage_transfer_service_account.subject_id)
+repo_url = Output.concat(
+    docker_registry.location,
+    '-docker.pkg.dev/',
+    gc_project,
+    '/',
+    docker_registry.repository_id,
+)
+
+
+image = Image(
+    "custom-python",
+    build={
+        'context': './docker/python/',
+        'dockerfile':'./docker/python/Dockerfile',
+        'platform': 'linux/amd64',
+    },
+    image_name=repo_url.apply(lambda url: f'{url}/custom-python312:v0.0.1'),
+    registry={
+        'server': repo_url.apply(lambda url: f'{url}'),
+    },
+)
+
+
+export('bucket_name', bucket.url)
+export('storage_transfer_service_account_email', storage_transfer_service_account.email)
+export('storage_transfer_service_account_subject_id', storage_transfer_service_account.subject_id)
+export('docker_registry_id', docker_registry.id)
+export("imageName", image.base_image_name)
