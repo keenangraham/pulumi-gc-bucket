f1a06a8364cb1d0e26f03f1dd7a0232b5d3267ee
Keenan Graham <ksgraham@stanford.edu>
2025-04-10 18:42:33 -0700

    Add service account details

--- __main__.py
+++ __main__.py
@@ -2,6 +2,10 @@ import pulumi
 
 
 from pulumi_gcp import storage
+from pulumi_gcp.storage import get_transfer_project_service_account
+
+
+service_transfer_service_account = get_transfer_project_service_account()
 
 
 bucket = storage.Bucket(
@@ -15,3 +19,5 @@ bucket = storage.Bucket(
 
 
 pulumi.export('bucket_name', bucket.url)
+pulumi.export('storage_transfer_service_account_email', service_transfer_service_account.email)
+pulumi.export('storage_transfer_service_account_subject_id', service_transfer_service_account.subject_id)
